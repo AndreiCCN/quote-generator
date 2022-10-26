088d50a0c5a7bbc188722f835eb2d41a45ba1aff
AndreiCCN <andreicuconoiu@gmail.com>
2022-10-26 20:58:12 +0200

    Fetch request to get and store all quotes

---
+++ script.js
@@ -0,0 +1,15 @@
+let apiQuotes = [];
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// On Load
+getQuotes();
